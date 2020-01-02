8e3d2117d87fda23cce68a37848524dbac8d407c
gus <angusgyoung@gmail.com>
2020-01-02 16:10:35 +0000

    add function to check and refresh auth token

--- src/helpers/authentication.js
+++ src/helpers/authentication.js
@@ -1,81 +1,111 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const authenticationHelper = {
-    login,
-    logout,
-    authHeader,
-    authHeaderString
+  login,
+  logout,
+  authHeader,
+  authHeaderString,
+  handleTokenExpiry
 };
 
 function authHeader() {
-    // return authorization header with jwt token
-    const token = userAccessToken();
-    if (token) {
-        return { Authorization: `Bearer ${token}` };
-    } else {
-        return {};
-    }
+  // return authorization header with jwt token
+  const token = userAccessToken();
+  if (token) {
+    return { Authorization: `Bearer ${token}` };
+  } else {
+    return {};
+  }
 }
 
 function authHeaderString() {
-    // return authorization header with jwt token
-    const token = userAccessToken();
-    if (token) {
-        return `Bearer ${token}`;
-    } else {
-        return '';
-    }
+  // return authorization header with jwt token
+  const token = userAccessToken();
+  if (token) {
+    return `Bearer ${token}`;
+  } else {
+    return "";
+  }
 }
 
 function handleResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if ([401, 403].indexOf(response.status) !== -1) {
-                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                logout();
-                window.location.reload(true);
-            }
+  return response.text().then(text => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if ([401, 403].indexOf(response.status) !== -1) {
+        // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+        logout();
+        window.location.reload(true);
+      }
 
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
 
-        return data;
-    });
+    return data;
+  });
 }
 
-
 function login(username, password) {
-    return fetch(`${API_URL}/auth`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-            'username': username,
-            'password': password
-        })
+  return fetch(`${API_URL}/auth`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
     })
+  })
     .then(handleResponse)
-    .then(user => {
+    .then(
+      user => {
         // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        console.debug('Authentication complete for user', user.profile.username);
+        localStorage.setItem("currentUser", JSON.stringify(user));
+        console.debug(
+          "Authentication complete for user",
+          user.profile.username
+        );
         return user;
-    }, 
-        error => {
-            console.debug('Authentication failed', error);
-    })   
+      },
+      error => {
+        console.debug("Authentication failed", error);
+      }
+    );
 }
 
 function logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('currentUser');
+  // remove user from local storage to log user out
+  localStorage.removeItem("currentUser");
 }
 
 function userAccessToken() {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'))
-    return currentUser.jwtToken;
+  let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  return currentUser.jwtToken;
+}
+
+// In a situation where our authentication token is close to expiring,
+// we should call the token refresh endpoint to get a new token
+async function handleTokenExpiry() {
+  let userDetails = JSON.parse(localStorage.getItem("currentUser"));
+  if (userDetails.expiryDate - new Date().getTime() / 1000 < 60) {
+    console.debug("Token is within expiry range, fetching new token");
+    let response = await fetch(`${API_URL}/auth/refresh`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: authHeaderString()
+      }
+    });
+    if (response) {
+      let newUserAuthDetails = await response.json();
+
+      userDetails.jwtToken = newUserAuthDetails.jwtToken;
+      userDetails.expiryDate = newUserAuthDetails.expiryDate;
+
+      // set the updated user details in local storage
+      localStorage.setItem("currentUser", JSON.stringify(userDetails));
+    }
+  }
 }
